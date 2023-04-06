89510f77037541912eb82c7c406275280296610a
Ridho Suhaebi Arrowi <94096357+Ridhorezi@users.noreply.github.com>
2023-04-06 13:44:31 +0700

    setup mobile & web responsive,and displaying contact mobile list

---
+++ lib/info.dart
@@ -0,0 +1,27 @@
+const info = [
+  {
+    'name': 'Ridho Suhaebi Arrowi',
+    'message' : 'Hey, how are you doing?',
+    'time' : '3:53 PM',
+    'profilePic' : 'https://iili.io/HOE4qxI.jpg',
+  },
+  {
+    'name': 'Giba Radia',
+    'message' : 'Im fine, and you?',
+    'time' : '4:53 PM',
+    'profilePic' : 'https://iili.io/HOE4ofn.jpg',
+  }
+];
+
+const messages = [
+  {
+    "isMe" : false,
+    "text" : "Hey what is up with you",
+    "time" : "10:00 AM",
+  },
+  {
+    "isMe" : true,
+    "text" : "Hey what is up with you",
+    "time" : "11:00 AM",
+  }
+];
