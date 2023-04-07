71b360c1330a4d22312005a264b22a378d4c4702
Ridho Suhaebi Arrowi <94096357+Ridhorezi@users.noreply.github.com>
2023-04-07 15:09:34 +0700

    Implement profile bar

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_ui_clone/widgets/contacts_list.dart';
+import 'package:whatsapp_ui_clone/widgets/web_profile_bar.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({super.key});
@@ -14,6 +15,7 @@ class WebScreenLayout extends StatelessWidget {
             child: SingleChildScrollView(
               child: Column(
                 children: const [
+                  WebProfileBar(),
                   ContactsList(),
                 ],
               ),
